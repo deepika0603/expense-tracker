3d6e08880730b99d8b6140806652afc9492b1ba5
deepika <deepika75013@gmail.com>
2025-06-12 12:44:43 +0530

    update amount logic

--- expense-tracker/src/components/modal/Add_Income_modal/AddIncomeModal.jsx
+++ expense-tracker/src/components/modal/Add_Income_modal/AddIncomeModal.jsx
@@ -4,58 +4,56 @@ import { useSnackbar } from "notistack";
 import MyContext from "../../../context";
 
 export default function AddIncomeModal({ setOpen }) {
-    const { walletBalance, setWalletBalance } = useContext(MyContext);
-  
-    const { enqueueSnackbar } = useSnackbar();
-  
-    //input on change state
-    const [incomeAmount, setIncomeAmount] = useState(null);
-  
-    //handling input on change and updating incomeAmount
-    const handleInputAmount = (e) => {
-      setIncomeAmount(Number(e.target.value));
-    };
-  
-    //handling wallet balance on clicking on add amount button
-    const handleAddAmount = () => {
-      //safety check if user is trying to add more than 1000000000 amount printing msg
-      if (incomeAmount + walletBalance > 1000000000) {
-        enqueueSnackbar("Cannot accept amount more than 1000000000", {
-          variant: "warning",
-        });
-        return;
-      }
-      const oldAmount = Number(localStorage.getItem("walletBalance"));
-      localStorage.setItem("walletBalance", oldAmount + incomeAmount);
-      setWalletBalance((prevAmount) => prevAmount + incomeAmount);
-      setOpen(false);
-    };
-  
-    //handle cancel button
-    const handleCancel = () => {
-      setOpen(false);
-    };
-  
-    return (
-      <div className={styles.container}>
-        <h1 className={styles.heading}>Add Balance</h1>
-        <div className={styles.wrapper}>
-          <input
-            type="number"
-            placeholder="Income Amount"
-            onChange={handleInputAmount}
-          />
-          <button
-            id={styles.add_balance_button}
-            type="button"
-            onClick={handleAddAmount}
-          >
-            Add Balance
-          </button>
-          <button type="button" onClick={handleCancel}>
-            Cancel
-          </button>
-        </div>
-      </div>
-    );
-  }
+  const { walletBalance, setWalletBalance } = useContext(MyContext);
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  //input on change state
+  const [incomeAmount, setIncomeAmount] = useState(null);
+
+  //handling input on change and updating incomeAmount
+  const handleInputAmount = (e) => {
+    setIncomeAmount(Number(e.target.value));
+  };
+
+  //handling wallet balance on clicking on add amount button
+  const handleAddAmount = (e) => {
+    //safety check if user is trying to add more than 1000000000 amount printing msg
+    if (incomeAmount + walletBalance > 1000000000) {
+      enqueueSnackbar("Cannot accept amount more than 1000000000", {
+        variant: "warning",
+      });
+      return;
+    }
+    const oldAmount = Number(localStorage.getItem("walletBalance"));
+    localStorage.setItem("walletBalance", oldAmount + incomeAmount);
+    setWalletBalance((prevAmount) => prevAmount + incomeAmount);
+    setOpen(false);
+    e.preventDefault();
+  };
+
+  //handle cancel button
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Add Balance</h1>
+      <form className={styles.wrapper} onSubmit={handleAddAmount}>
+        <input
+          type="number"
+          placeholder="Income Amount"
+          onChange={handleInputAmount}
+          required
+        />
+        <button id={styles.add_balance_button} type="submit">
+          Add Balance
+        </button>
+        <button type="button" onClick={handleCancel}>
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+}
